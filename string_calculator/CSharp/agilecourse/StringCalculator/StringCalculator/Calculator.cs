6f97616436f53e7c6d7f2d5c22bc6269287b5c57
Juri <juri.strumpflohner@gmail.com>
2013-04-17 22:48:02 +0200

    added string calculator kata exercise done during agile course

---
+++ string_calculator/CSharp/agilecourse/StringCalculator/StringCalculator/Calculator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace StringCalculator
+{
+    public static class Calculator
+    {
+        static string regularexpression = "//.*\\n";
+
+        public static int Add(string value)
+        {
+            if (String.IsNullOrEmpty(value))
+                return 0;
+
+            string delimiter = ",";
+            if (Regex.IsMatch(value, regularexpression))
+            {
+                delimiter = value.Substring(2, 1);
+                value = Regex.Replace(value, regularexpression, "");
+            }
+
+            List<int> negativeNumbers = new List<int>();
+            string[] splitted = value.Split(new string[] { delimiter, "\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+            int result = 0;
+            foreach (string val in splitted)
+            {
+                int convertedVal = Int32.Parse(val);
+
+                if (convertedVal < 0)
+                {
+                    negativeNumbers.Add(convertedVal);
+                }
+                else if (convertedVal < 1000)
+                {
+                    result += convertedVal;
+                }
+            }
+
+            if (negativeNumbers.Count > 0)
+            {
+                throw new Exception(String.Format("Negative numbers not allowed: {0}", String.Join(",", negativeNumbers)));
+            }
+
+            return result;
+        }
+
+    }
+}
---
+++ string_calculator/CSharp/agilecourse/StringCalculator/StringCalculatorTest/CalculatorTest.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringCalculator;
+
+namespace StringCalculatorTest
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void ShouldReturnZeroWhenPassingEmptyString()
+        {
+            Assert.AreEqual(0, Calculator.Add(""));
+        }
+
+        [TestMethod]
+        public void ShouldReturnASingleNumber()
+        {
+            Assert.AreEqual(1, Calculator.Add("1"));
+            Assert.AreEqual(2, Calculator.Add("2"));            
+        }
+
+        [TestMethod]
+        public void ShouldAddTwoPositiveNumbers()
+        {
+            Assert.AreEqual(3, Calculator.Add("1,2"));
+            Assert.AreEqual(14, Calculator.Add("3,11"));            
+        }
+
+        [TestMethod]
+        public void ShouldAddTwoOrMoreNumbers()
+        {
+            Assert.AreEqual(6, Calculator.Add("3,1,2"));
+            Assert.AreEqual(11, Calculator.Add("3,1,2,5"));            
+        }
+
+        [TestMethod]
+        public void ShouldAcceptNewlineCharacter()
+        {
+            Assert.AreEqual(6, Calculator.Add("1\n2,3"));            
+        }
+
+        [TestMethod]
+        public void ShouldThrowExceptionOnNegativeNumbers()
+        {
+            ShouldThrowException("-1", "-1");
+            ShouldThrowException("-1,-2", "-1,-2");
+            ShouldThrowException("-1,3,-2", "-1,-2");
+        }
+
+        [TestMethod]        
+        public void ShouldThrowExceptionOnNegativeNumbers1()
+        {
+            ShouldThrowException("-1", "-1");
+            ShouldThrowException("-1,-2", "-1,-2");
+            ShouldThrowException("-1,3,-2", "-1,-2");
+        }
+
+        [TestMethod]
+        public void ShouldAllowUsingADifferentDelimiter()
+        {
+            Assert.AreEqual(3, Calculator.Add("//;\n1;2"));
+        }
+
+        [TestMethod]
+        public void ShouldIgnoreNumbersBiggerThan1000()
+        {
+            Assert.AreEqual(2, Calculator.Add("2,1001"));
+        }
+
+        [TestMethod]
+        public void ShouldAcceptDelimitersWithAVariableLength()
+        {
+            Assert.AreEqual(6, Calculator.Add("//***\n1***2***3"));
+        }
+
+        private void ShouldThrowException(string calculatorString, string expectedNegatives)
+        {
+            bool exceptionThrown = false;
+            try
+            {
+                Calculator.Add(calculatorString);
+            }
+            catch (Exception e)
+            {
+                exceptionThrown = true;
+                Assert.AreEqual("Negative numbers not allowed: " + expectedNegatives, e.Message);
+            }
+            Assert.IsTrue(exceptionThrown);
+        }
+
+    }
+}
