827e6c8beaa358b58842b44cfeb5502e6ee9d85a
Juri <juri.strumpflohner@gmail.com>
2013-05-03 13:52:13 +0200

    test: passing 2 should return 2

--- string_calculator/Java/run1/CalculatorKata/tests/CalculatorTest.java
+++ string_calculator/Java/run1/CalculatorKata/tests/CalculatorTest.java
@@ -4,11 +4,10 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-
 public class CalculatorTest {
 
 	private Calculator calculator;
-	
+
 	@Before
 	public void setUp() throws Exception {
 		this.calculator = new Calculator();
@@ -19,19 +18,24 @@ public class CalculatorTest {
 		this.calculator = null;
 	}
 
-    @Test
-    public void shouldReturn0WhenPassingEmptyString() {
-        assertEquals(0, calculator.add(""));        
-    }
-    
-    @Test
-    public void shouldReturn0WhenPassingNull()  {
-        assertEquals(0, calculator.add(null));
-    }
-    
-    @Test
-    public void shouldReturn1WhenPassing1(){
-        assertEquals(1, calculator.add("1"));        
-    }
+	@Test
+	public void shouldReturn0WhenPassingEmptyString() {
+		assertEquals(0, calculator.add(""));
+	}
+
+	@Test
+	public void shouldReturn0WhenPassingNull() {
+		assertEquals(0, calculator.add(null));
+	}
+
+	@Test
+	public void shouldReturn1WhenPassing1() {
+		assertEquals(1, calculator.add("1"));
+	}
 	
+	@Test
+	public void shouldReturn2WhenPassing2() {
+		assertEquals(2, calculator.add("2"));
+	}
+
 }
