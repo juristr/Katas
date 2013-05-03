d58e3b047927cfa627bedd832c5c6015546fb78f
Juri <juri.strumpflohner@gmail.com>
2013-05-03 13:32:39 +0200

    adding unit test for Calculator

---
+++ string_calculator/Java/run1/CalculatorKata/tests/CalculatorTest.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class CalculatorTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+}
