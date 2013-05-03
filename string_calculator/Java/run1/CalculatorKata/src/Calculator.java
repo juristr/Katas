51a10f95131800b103fcdcfbd1a477788b0673ad
Juri <juri.strumpflohner@gmail.com>
2013-05-03 13:35:50 +0200

    impl: passing 1 should return 1

--- string_calculator/Java/run1/CalculatorKata/src/Calculator.java
+++ string_calculator/Java/run1/CalculatorKata/src/Calculator.java
@@ -2,8 +2,13 @@
 public class Calculator {
 
 	public int add(String string) {
-		// TODO Auto-generated method stub
-		return 0;
+		if(string == null)
+			return 0;
+		
+		if(string == "")
+			return 0;
+		
+		return 1;
 	}
 
 }
